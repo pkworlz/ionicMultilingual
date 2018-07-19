559ab4779990540b96797ac0aa0b6347d576b827
Kumar Nitesh <development@menumate.com>
2018-07-19 15:23:17 +0530

    project added..

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = TabsPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,translate: TranslateService) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      translate.setDefaultLang('en');
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { TranslateService } from '@ngx-translate/core';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,public translateService: TranslateService) {
+
+  }
+  changeLanguage(langauge){
+    this.translateService.use(langauge);
+  }
+
+}
